81cb08ddda286087f131ab42ef498c05c5af61bb
Aishnag <87310022+Aishnag@users.noreply.github.com>
2022-01-16 06:58:47 +0000

    Add files via upload
    Different method code committed.

---
+++ DiiferentMethodsDemo.java
@@ -0,0 +1,15 @@
+package com.differntmethods;
+// different methods with differnt return type.
+public class DiiferentMethodsDemo {
+	// int is  return type  of additionOfNumber.
+	public  static  int additionOfNumber(int a,int b) {
+		int c = a+b;
+		return c;
+	}
+	public static void main(String[] args) {
+		
+	int addition = additionOfNumber(5, 4);
+	System.out.println("Addition of two number is:"+addition);
+	}
+
+}
